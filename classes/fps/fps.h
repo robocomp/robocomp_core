24586b60394766d85a2e4a96f21f93536d8126a4
pbustos <pbustos@unex.es>
2023-06-23 22:03:44 +0200

    added mem and cpu

--- classes/fps/fps.h
+++ classes/fps/fps.h
@@ -1,4 +1,13 @@
+//
 // Example: fps.print("FPS:");
+// default refresh period is 1000
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 class FPSCounter
 {
@@ -6,6 +15,20 @@ class FPSCounter
 		FPSCounter()
 		{
 			begin = std::chrono::high_resolution_clock::now();
+            FILE* file;
+            struct tms timeSample;
+            char line[128];
+
+            lastCPU = times(&timeSample);
+            lastSysCPU = timeSample.tms_stime;
+            lastUserCPU = timeSample.tms_utime;
+
+            file = fopen("/proc/cpuinfo", "r");
+            numProcessors = 0;
+            while(fgets(line, 128, file) != NULL){
+                if (strncmp(line, "processor", 9) == 0) numProcessors++;
+            }
+            fclose(file);
 		}
         int print( const std::string &text, const unsigned int msPeriod = 1000)
         {
@@ -15,7 +38,10 @@ class FPSCounter
             if( elapsed > msPeriod)
             {
 				last_period = elapsed/cont;
-                std::cout << "Epoch time = " << last_period << "ms. Fps = " << cont << " " << text << std::endl;
+                float cpu = get_cpu_use();
+                int mem = get_mem_use();
+                std::cout << "Period = " << last_period << "ms. Fps = " << cont << " " << text
+                          << " cpu = " << cpu << "%" << " mem = " << mem << "MB" << std::endl;
                 begin = std::chrono::high_resolution_clock::now();
                 fps=cont;
                 cont = 0;
@@ -30,17 +56,77 @@ class FPSCounter
 			if( elapsed > msPeriod)
 			{
 				last_period = elapsed/cont;
-				std::cout << "Epoch time = " << last_period << "ms. Fps = " << cont << " " << text << std::endl;
+                float cpu = get_cpu_use();
+                int mem = get_mem_use();
+                std::cout << "Period = " << last_period << "ms. Fps = " << cont << " " << text
+                          << " cpu = " << cpu << "%" << " mem = " << mem << "MB" << std::endl;
 				begin = std::chrono::high_resolution_clock::now();
 				f(cont);
 				cont = 0;
 			}
 			cont++;
 		}
-		float get_period() const {return last_period;}
+		float get_period() const
+        {
+            return last_period;
+        }
 
+        float get_cpu_use()
+        {
+            struct tms timeSample;
+            clock_t now;
+            double percent;
+
+            now = times(&timeSample);
+            if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+                timeSample.tms_utime < lastUserCPU)
+            {
+                //Overflow detection. Just skip this value.
+                percent = -1.0;
+            }
+            else
+            {
+                percent = (timeSample.tms_stime - lastSysCPU) +
+                          (timeSample.tms_utime - lastUserCPU);
+                percent /= (now - lastCPU);
+                percent *= 100;
+            }
+            lastCPU = now;
+            lastSysCPU = timeSample.tms_stime;
+            lastUserCPU = timeSample.tms_utime;
+            return (int)percent;
+        }
+        int get_mem_use()
+        { //Note: this value is in MB!
+            FILE* file = fopen("/proc/self/status", "r");
+            int result = -1;
+            char line[128];
+
+            while (fgets(line, 128, file) != NULL)
+            {
+                if (strncmp(line, "VmRSS:", 6) == 0)
+                {
+                    result = parseLine(line);
+                    break;
+                }
+            }
+            fclose(file);
+            return result/1000;
+        }
+        int parseLine(char* line)
+        {
+            // This assumes that a digit will be found and the line ends in " Kb".
+            int i = strlen(line);
+            const char* p = line;
+            while (*p <'0' || *p > '9') p++;
+            line[i-3] = '\0';
+            i = atoi(p);
+            return i;
+        }
 		std::chrono::time_point<std::chrono::high_resolution_clock> begin;
 		int cont = 0;
 		float last_period = 0;
+        clock_t lastCPU, lastSysCPU, lastUserCPU;
+        int numProcessors;
 };
 

4652dc5854c356c1d51609bbe757121c702775e5
pbustos <pbustos@unex.es>
2023-12-28 21:32:56 +0100

    added is_empty method

--- classes/doublebuffer/DoubleBuffer.h
+++ classes/doublebuffer/DoubleBuffer.h
@@ -44,156 +44,162 @@ struct is_iterable<T, std::void_t<decltype(std::declval<T>().begin()), decltype(
 template <class I, class O>
 class DoubleBuffer
 {
-private:
-    mutable std::shared_mutex bufferMutex;
-    std::condition_variable_any cv;
-    std::chrono::microseconds write_freq;
-    std::chrono::time_point<std::chrono::steady_clock>  last_write;
+    private:
+        mutable std::shared_mutex bufferMutex;
+        std::condition_variable_any cv;
+        std::chrono::microseconds write_freq;
+        std::chrono::time_point<std::chrono::steady_clock>  last_write;
 
-    O bufferA;
-    O bufferB;
-    O &readBuffer;// = bufferA;
-    O &writeBuffer;// = bufferB;
-    std::atomic_bool empty;// = true;
-    ThreadPool worker;
+        O bufferA;
+        O bufferB;
+        O &readBuffer;// = bufferA;
+        O &writeBuffer;// = bufferB;
+        std::atomic_bool empty;// = true;
+        ThreadPool worker;
 
-public:
-    DoubleBuffer() : write_freq(0us), readBuffer(bufferA), writeBuffer(bufferB), empty(true), worker(1) {};
-    explicit DoubleBuffer(std::chrono::milliseconds t) : write_freq(std::chrono::duration_cast<std::chrono::microseconds>(t)),
-                                                         readBuffer(bufferA), writeBuffer(bufferB), empty(true),
-                                                         worker(1) {};
+    public:
+        DoubleBuffer() : write_freq(0us), readBuffer(bufferA), writeBuffer(bufferB), empty(true), worker(1) {};
+        explicit DoubleBuffer(std::chrono::milliseconds t) : write_freq(std::chrono::duration_cast<std::chrono::microseconds>(t)),
+                                                             readBuffer(bufferA), writeBuffer(bufferB), empty(true),
+                                                             worker(1) {};
 
-    ~DoubleBuffer() {};
-    void set_write_freq(const std::chrono::milliseconds &freq)
-    {
-        write_freq = std::chrono::duration_cast<std::chrono::microseconds>(freq);
-    };
-
-    // Waits for the buffer to be filled before retrieving its content
-    O get(std::chrono::milliseconds t = 200ms ) {
-        std::shared_lock lock(bufferMutex);
-
-        //cambiar esto cuando se implemente atomic wait/notify
-        if (!cv.wait_until(bufferMutex,
-                           std::chrono::steady_clock::now() + t ,
-                           [this]() { return !empty.load();})){
-            throw std::runtime_error("Timeout");
-        }
-        empty.store(true);
-        return readBuffer;
-    }
-
-   // Retrieves the buffer's content without setting it empty or checking that it is filled up
-   O get_idemp(std::chrono::milliseconds t = 200ms )
-   {
-        std::shared_lock lock(bufferMutex);
-       if (!cv.wait_until(bufferMutex,
-                          std::chrono::steady_clock::now() + t ,
-                          [this]() { return !empty.load();})){
-           throw std::runtime_error("Timeout");
-       }
-        return readBuffer;
-   }
-
-    std::optional<O> try_get()
-    {
-        if (empty.load()){
-            return {};
-        }
-
-        std::shared_lock lock(bufferMutex);
-        empty.store(true);
-        return readBuffer;
-    }
-
-    /*
-    bool put(I d, std::function<void(I &&, O &)> t = empty_fn)
-    {
-        auto now = std::chrono::steady_clock::now();
-        if (std::chrono::duration_cast<std::chrono::microseconds>(now - last_write)  > write_freq) {
-
-            last_write = now;
-            worker.spawn_task([&, this, d = std::move(d), t = std::move(t)](){
-                O temp;
-                if (this->ItoO(std::move(d), temp, t))
-                {
-                    std::unique_lock lock(this->bufferMutex);
-                    this->writeBuffer = std::move(temp);
-                    std::swap(this->writeBuffer, this->readBuffer);
-                    this->empty.store(false);
-                    this->cv.notify_all();
-
-                }
-            });
-
-            return true;
-        } else {
-            return false;
-        };
-    }*/
-
-    bool put(I &&d, std::function<void(I &&, O &)> t = empty_fn)
-    {
-        auto now = std::chrono::steady_clock::now();
-        if (std::chrono::duration_cast<std::chrono::microseconds>(now - last_write)  > write_freq) {
-
-            last_write = now;
-            worker.spawn_task([&, this, d = std::move(d), t = std::move(t)]() mutable {
-                O temp;
-                if (this->ItoO(std::move(d), temp, t))
-                {
-                    std::unique_lock lock(this->bufferMutex);
-                    this->writeBuffer = std::move(temp);
-                    std::swap(writeBuffer, readBuffer);
-                    empty.store(false);
-                    cv.notify_all();
-                }
-            });
-
-            return true;
-        } else  {
-            return false;
-        };
-    }
-
-private:
-
-    bool ItoO(I &&iTypeData, O &oTypeData, const std::function<void( I &&, O &)>& t = empty_fn)
-    {
-        //Si es el mismo tipo o es convertible de I a O
-        if constexpr (std::is_same<I, O>::value || std::is_convertible<I, O>::value)
+        ~DoubleBuffer() {};
+        void set_write_freq(const std::chrono::milliseconds &freq)
         {
-            oTypeData = std::move(iTypeData);
+            write_freq = std::chrono::duration_cast<std::chrono::microseconds>(freq);
+        };
+
+        // Waits for the buffer to be filled before retrieving its content
+        O get(std::chrono::milliseconds t = 200ms ) {
+            std::shared_lock lock(bufferMutex);
+
+            //cambiar esto cuando se implemente atomic wait/notify
+            if (!cv.wait_until(bufferMutex,
+                               std::chrono::steady_clock::now() + t ,
+                               [this]() { return !empty.load();})){
+                throw std::runtime_error("Timeout");
+            }
+            empty.store(true);
+            return readBuffer;
         }
-        else if constexpr (is_iterable<I>::value && is_iterable<O>::value)
-        { //Si es iterable, hay que ver si esto es suficiente
 
-            //Conseguimos el tipo que contiene el iterador
-            using I_T = typename std::decay<decltype(*iTypeData.begin())>::type;
-            using O_T = typename std::decay<decltype(*oTypeData.begin())>::type;
+       // Retrieves the buffer's content without setting it empty or checking that it is filled up
+       O get_idemp(std::chrono::milliseconds t = 200ms )
+       {
+            std::shared_lock lock(bufferMutex);
+           if (!cv.wait_until(bufferMutex,
+                              std::chrono::steady_clock::now() + t ,
+                              [this]() { return !empty.load();})){
+               throw std::runtime_error("Timeout");
+           }
+            return readBuffer;
+       }
 
-            //Comprobamos si se puede convertir el I_T a O_T
-            if constexpr (std::is_convertible<I_T, O_T>::value)
+       std::optional<O> try_get()
+       {
+           if (empty.load()){
+               return {};
+           }
+
+           std::shared_lock lock(bufferMutex);
+           empty.store(true);
+           return readBuffer;
+       }
+
+       // checks if the buffer is filled up
+       bool is_empty() const
+       {
+           return empty.load();
+       }
+
+        /*
+        bool put(I d, std::function<void(I &&, O &)> t = empty_fn)
+        {
+            auto now = std::chrono::steady_clock::now();
+            if (std::chrono::duration_cast<std::chrono::microseconds>(now - last_write)  > write_freq) {
+
+                last_write = now;
+                worker.spawn_task([&, this, d = std::move(d), t = std::move(t)](){
+                    O temp;
+                    if (this->ItoO(std::move(d), temp, t))
+                    {
+                        std::unique_lock lock(this->bufferMutex);
+                        this->writeBuffer = std::move(temp);
+                        std::swap(this->writeBuffer, this->readBuffer);
+                        this->empty.store(false);
+                        this->cv.notify_all();
+
+                    }
+                });
+
+                return true;
+            } else {
+                return false;
+            };
+        }*/
+
+        bool put(I &&d, std::function<void(I &&, O &)> t = empty_fn)
+        {
+            auto now = std::chrono::steady_clock::now();
+            if (std::chrono::duration_cast<std::chrono::microseconds>(now - last_write)  > write_freq) {
+
+                last_write = now;
+                worker.spawn_task([&, this, d = std::move(d), t = std::move(t)]() mutable {
+                    O temp;
+                    if (this->ItoO(std::move(d), temp, t))
+                    {
+                        std::unique_lock lock(this->bufferMutex);
+                        this->writeBuffer = std::move(temp);
+                        std::swap(writeBuffer, readBuffer);
+                        empty.store(false);
+                        cv.notify_all();
+                    }
+                });
+
+                return true;
+            } else  {
+                return false;
+            };
+        }
+
+    private:
+
+        bool ItoO(I &&iTypeData, O &oTypeData, const std::function<void( I &&, O &)>& t = empty_fn)
+        {
+            //Si es el mismo tipo o es convertible de I a O
+            if constexpr (std::is_same<I, O>::value || std::is_convertible<I, O>::value)
             {
-                oTypeData = O(std::make_move_iterator(iTypeData.begin()), std::make_move_iterator(iTypeData.end()));
+                oTypeData = std::move(iTypeData);
+            }
+            else if constexpr (is_iterable<I>::value && is_iterable<O>::value)
+            { //Si es iterable, hay que ver si esto es suficiente
+
+                //Conseguimos el tipo que contiene el iterador
+                using I_T = typename std::decay<decltype(*iTypeData.begin())>::type;
+                using O_T = typename std::decay<decltype(*oTypeData.begin())>::type;
+
+                //Comprobamos si se puede convertir el I_T a O_T
+                if constexpr (std::is_convertible<I_T, O_T>::value)
+                {
+                    oTypeData = O(std::make_move_iterator(iTypeData.begin()), std::make_move_iterator(iTypeData.end()));
+                }
+                else
+                {
+                    //Si no es convertible necesitamos una función para convertir de I a O.
+                    //Comprobamos que el tipo de la función t es distinto del tipo de empty_fn (Las lambdas tienen tipos únicos).
+                    static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value, "A function needs to be implemented to transform ItoO");
+                    //Llamamos a la función
+                    t(std::move(iTypeData), oTypeData);
+                }
             }
             else
             {
-                //Si no es convertible necesitamos una función para convertir de I a O.
-                //Comprobamos que el tipo de la función t es distinto del tipo de empty_fn (Las lambdas tienen tipos únicos).
+                //Lo mismo que antes.
                 static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value, "A function needs to be implemented to transform ItoO");
-                //Llamamos a la función
                 t(std::move(iTypeData), oTypeData);
             }
-        }
-        else
-        {
-            //Lo mismo que antes.
-            static_assert(!std::is_same<decltype(t), decltype(empty_fn)>::value, "A function needs to be implemented to transform ItoO");
-            t(std::move(iTypeData), oTypeData);
-        }
-        return true;
-    };
+            return true;
+        };
 };
 
 #endif

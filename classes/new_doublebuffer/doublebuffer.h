5fff29dd3f195d86d5cc18c2046a052b051feb42
pbustos <pbustos@unex.es>
2024-06-02 19:33:10 +0200

    fixes

--- classes/new_doublebuffer/doublebuffer.h
+++ classes/new_doublebuffer/doublebuffer.h
@@ -1,3 +1,5 @@
+// DounbleBuffer class template definition
+
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -9,8 +11,30 @@
 #include <functional>
 #include <queue>
 #include <future>
+#include <optional>
 //#include <boost/lockfree/queue.hpp>
 #include "../threadpool/threadpool.h" // Include your ThreadPool header
+
+// concept Printable to check if the type can be printed to std::cout
+template <typename T>
+concept Printable = requires(T t)
+{
+    { std::cout << t } -> std::same_as<std::ostream &>;
+};
+
+// concept AllPrintable to check if all types in a tuple can be printed
+template <typename... Args>
+concept AllPrintable = (Printable<Args> && ...);
+
+// Default empty function for input to output transformation
+constexpr auto empty_fn = [](auto &&I, auto &T) {};
+
+// Check if a type is iterable
+template <typename T, typename = void>
+struct is_iterable : std::false_type {};
+
+template <typename  T>
+struct is_iterable<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end())>> : std::true_type {};
 
 // Primary template (never used directly)
 template <typename InputTypes, typename OutputTypes>
@@ -19,79 +43,181 @@ class DoubleBuffer;
 template <typename... InputTypes, typename... OutputTypes>
 class DoubleBuffer<std::tuple<InputTypes...>, std::tuple<OutputTypes...>>
 {
-public:
-    struct DataElement
-    {
-        std::chrono::steady_clock::time_point timestamp;
-        std::tuple<OutputTypes...> data;
-    };
-
-    DoubleBuffer(size_t size, size_t threadPoolSize)
-            : buffer(size), bufferSize(size), head(0), count(0), stopWorker(false), threadPool(threadPoolSize)
-    {
-    }
-
-    ~DoubleBuffer()
-    {
-        stopWorker = true;
-    }
-
-    // Producer puts input data into the buffer queue with a transformation function
-    void put(std::tuple<InputTypes...> &&inputs, std::function<std::tuple<OutputTypes...>(std::tuple<InputTypes...> &&)> transform)
-    {
-        // Submit the transformation task to the thread pool
-        threadPool.spawn_task([this, inputs = std::move(inputs), transform = std::move(transform)]() mutable
-                              {
-                                  auto transformedData = transform(std::move(inputs));
-                                  auto timestamp = std::chrono::steady_clock::now();
-
-                                  {
-                                      std::unique_lock<std::mutex> lock(mtx);
-                                      buffer[head] = {timestamp, std::move(transformedData)};
-                                      head = (head + 1) % bufferSize;
-                                      if (count < bufferSize)
-                                          ++count;
-                                  }
-                                  cv.notify_all(); });
-    }
-
-    // Consumer gets data closest to the given timestamp (or the most recent if timestamp is zero)
-    std::tuple<OutputTypes...> get(const std::chrono::steady_clock::time_point &targetTime = std::chrono::steady_clock::time_point::min())
-    {
-        std::unique_lock<std::mutex> lock(mtx);
-        cv.wait(lock, [this]()
-        { return count > 0; });
-
-        if (targetTime == std::chrono::steady_clock::time_point::min())
+    public:
+        struct DataElement
         {
-            // Return the most recent element
-            return buffer[(head + bufferSize - 1) % bufferSize].data;
+            std::chrono::steady_clock::time_point timestamp;
+            std::tuple<OutputTypes...> data;
+        };
+
+        DoubleBuffer() : buffer(1), bufferSize(1), head(0), count(0), threadPool(1) {};
+        DoubleBuffer(size_t size, size_t threadPoolSize)
+                : buffer(size), bufferSize(size), head(0), count(0), threadPool(threadPoolSize)
+        {
+            if (size == 0)
+                throw std::invalid_argument("Buffer size must be greater than zero");
+            if (threadPoolSize == 0)
+                throw std::invalid_argument("Thread pool size must be greater than zero");
+        };
+        ~DoubleBuffer() {};
+        void set_buffer_size(size_t size)
+        {
+            if (size == 0)
+                throw std::invalid_argument("Buffer size must be greater than zero");
+            bufferSize = size;
+            buffer.resize(size);
         }
 
-        size_t closestIndex = 0;
-        auto minDiff = std::chrono::steady_clock::duration::max();
-
-        for (size_t i = 0; i < count; ++i)
+        /// Producer puts input data into the buffer queue with a transformation function
+        void put(std::tuple<InputTypes...> &&inputs, std::function<std::tuple<OutputTypes...>(std::tuple<InputTypes...> &&)> transform)
         {
-            size_t index = (head + bufferSize - i - 1) % bufferSize;
-            auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(buffer[index].timestamp - targetTime);
-            if (std::abs(diff.count()) < std::abs(std::chrono::duration_cast<std::chrono::milliseconds>(minDiff).count()))
+            // Submit the transformation task to the thread pool
+            threadPool.spawn_task([this, inputs = std::move(inputs), transform = std::move(transform)]() mutable
+                                  {
+                                      std::tuple<OutputTypes...> temp;
+                                      if(this->convert_is_possible(std::move(inputs), temp, transform))
+                                      {
+                                          auto transformedData = transform(std::move(inputs));
+                                          auto timestamp = std::chrono::steady_clock::now();
+                                          std::unique_lock<std::mutex> lock(mtx);
+                                          buffer[head] = {timestamp, std::move(transformedData)};
+                                          head = (head + 1) % bufferSize;
+                                          if (count < bufferSize)
+                                              ++count;
+                                          cv.notify_all();
+                                      }
+                                  });
+        }
+
+        /// Consumer requests data closest to the given timestamp (or the most recent if timestamp is zero)
+        std::tuple<OutputTypes...> get(const std::chrono::steady_clock::time_point &targetTime = std::chrono::steady_clock::time_point::min())
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [this]()
+            { return count > 0; });
+
+            if (targetTime == std::chrono::steady_clock::time_point::min())
             {
-                closestIndex = index;
-                minDiff = diff;
+                // Return the most recent element
+                return buffer[(head + bufferSize - 1) % bufferSize].data;
+            }
+
+            size_t closestIndex = 0;
+            auto minDiff = std::chrono::steady_clock::duration::max();
+
+            for (size_t i = 0; i < count; ++i)
+            {
+                size_t index = (head + bufferSize - i - 1) % bufferSize;
+                auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(buffer[index].timestamp - targetTime);
+                if (std::abs(diff.count()) < std::abs(std::chrono::duration_cast<std::chrono::milliseconds>(minDiff).count()))
+                {
+                    closestIndex = index;
+                    minDiff = diff;
+                }
+            }
+
+            return buffer[closestIndex].data;
+        }
+
+        /// Consumer requests a new, fresh data given the timestamp of its latest read
+        std::optional<std::tuple<OutputTypes...>> get_new(const std::chrono::steady_clock::time_point &lastTime)
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [this]()
+            { return count > 0; });
+
+            // if lastTime is newer than the most recent data, return most recent data, else return empty tuple
+            if (lastTime >= buffer[(head + bufferSize - 1) % bufferSize].timestamp)
+                return buffer[(head + bufferSize - 1) % bufferSize].data;
+            else    // return empty tuple
+                return {};
+        }
+
+        /// Consumer requests all data newer the timestamp of its latest read
+        std::vector<std::tuple<OutputTypes...>> get_all_new(const std::chrono::steady_clock::time_point &lastTime)
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [this]()
+            { return count > 0; });
+
+            std::vector<std::tuple<OutputTypes...>> newData;
+           for (int i = count - 1; i >= 0; --i) // iterate from the most recent data backwards so they are inserted in the correct order
+           {
+                size_t index = (head + bufferSize - i - 1) % bufferSize;
+                if (buffer[index].timestamp > lastTime)
+                    newData.push_back(buffer[index].data);
+           }
+            return newData;
+        }
+
+        /// Prints the current state of the buffer with timestamps
+        void print() const requires AllPrintable<OutputTypes...>
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            std::cout << "[print] Buffer state: \n";
+            for (size_t i = 0; i < count; ++i)
+            {
+                size_t index = (head + bufferSize - i - 1) % bufferSize;
+                auto ts = std::chrono::duration_cast<std::chrono::milliseconds>(buffer[index].timestamp.time_since_epoch()).count();
+                std::cout << "  " << ts << " ms, Data: [";
+                printTuple(buffer[index].data);
+                std::cout << "]\n";
             }
         }
 
-        return buffer[closestIndex].data;
-    }
+    private:
+        std::vector<DataElement> buffer;
+        size_t bufferSize;
+        size_t head;
+        size_t count;
+        mutable std::mutex mtx;
+        std::condition_variable cv;
+        bool stopWorker;
+        ThreadPool threadPool;
 
-private:
-    std::vector<DataElement> buffer;
-    size_t bufferSize;
-    size_t head;
-    size_t count;
-    std::mutex mtx;
-    std::condition_variable cv;
-    bool stopWorker;
-    ThreadPool threadPool;
+        template <std::size_t Index = 0>
+        void printTuple(const std::tuple<OutputTypes...> &t) const
+        {
+            if constexpr (Index < sizeof...(OutputTypes))
+            {
+                std::cout << std::get<Index>(t) << " ";
+                printTuple<Index + 1>(t);
+            }
+        };
+
+    // Function to check if input types are convertible to output types
+
+    template <typename... Input, typename... Output>
+    bool convert_is_possible(std::tuple<InputTypes...> &&iTypeData, std::tuple<OutputTypes...> &oTypeData,
+                             const std::function<std::tuple<OutputTypes...>(std::tuple<InputTypes...> &&)> &transform = empty_fn)
+    {
+        ([&](auto &&input, auto &output)
+        {
+            using I_T = std::decay_t<decltype(input)>;
+            using O_T = std::decay_t<decltype(output)>;
+
+            if constexpr (std::is_same_v<I_T, O_T> || std::is_convertible_v<I_T, O_T>)
+                output = std::forward<I_T>(input);
+            else if constexpr (is_iterable<I_T>::value && is_iterable<O_T>::value)
+            {
+                using I_T_Element = typename std::decay<decltype(*std::begin(input))>::type;
+                using O_T_Element = typename std::decay<decltype(*std::begin(output))>::type;
+                if constexpr (std::is_convertible_v<I_T_Element, O_T_Element>)
+                {
+                    output = O_T(std::make_move_iterator(std::begin(input)), std::make_move_iterator(std::end(input)));
+                }
+                else
+                {
+                    static_assert(!std::is_same_v<decltype(transform), decltype(empty_fn)>, "A function needs to be implemented to transform input to output types");
+                    transform(std::forward<I_T>(input), output);
+                }
+            }
+            else
+            {
+                static_assert(!std::is_same_v<decltype(transform), decltype(empty_fn)>, "A function needs to be implemented to transform input to output types");
+                transform(std::forward<I_T>(input), output);
+            } }(std::forward<Input>(iTypeData), oTypeData),
+                ...);
+        return true;
+    }
 };
--- classes/new_doublebuffer/main.cpp
+++ classes/new_doublebuffer/main.cpp
@@ -1,6 +1,7 @@
 #include "doublebuffer.h"
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 // Helper function to create the transform lambda
 auto createTransform()
@@ -20,7 +21,21 @@ void producer(DoubleBuffer<std::tuple<int, float>, std::tuple<std::string, std::
     for (int i = 0; i < 10; ++i)
     {
         std::tuple<int, float> data = std::make_tuple(i, i * i);
-        buffer.put(std::move(data), createTransform());
+        buffer.put(std::move(data), [](std::tuple<int, float> &&inputs) -> std::tuple<std::string, std::string>
+        {
+            int i;
+            float f;
+            std::tie(i, f) = inputs;
+
+            return std::make_tuple("Int: " + std::to_string(i), "Float: " + std::to_string(f));
+        });
+
+        // Get current time as a time_point object
+        auto now = std::chrono::system_clock::now();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+        std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+        std::tm* localTime = std::localtime(&currentTime);
+        std::cout << "Producer: " << i << " at time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
@@ -30,30 +45,54 @@ void consumer(DoubleBuffer<std::tuple<int, float>, std::tuple<std::string, std::
     using namespace std::chrono_literals;
     for (int i = 0; i < 10; ++i)
     {
+        buffer.print();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+
         // Get the most recent data when target time is not specified
-        auto data = buffer.get();
-        std::cout << "Read most recent data: ";
-        std::apply([](auto &&... args)
-                   { ((std::cout << args << " "), ...); },
-                   data);
-        std::cout << std::endl;
-        //std::this_thread::sleep_for(std::chrono::milliseconds(150));
+        //static auto now = std::chrono::steady_clock::now();
+        auto now = std::chrono::steady_clock::now();
+        auto data_ = buffer.get_new(now);
+//        if(not data.empty())
+//            now = std::chrono::steady_clock::now();
+
+        //auto time =  ms.count();
+//        for(const auto &d : data)
+//        {
+//            std::cout << "\nRead most recent data at " << ms <<": ";
+//            std::apply([](auto &&... args)
+//                       { ((std::cout << args << " "), ...); },
+//                       d);
+//            std::cout << std::endl;
+//        }
+        if(data_.has_value())
+        {
+            std::cout << "Read most recent data at " << ms << ": ";
+            std::apply([](auto &&... args)
+                       { ((std::cout << args << " "), ...); },
+                       data_.value());
+            std::cout << std::endl;
+        }
 
         // Get data closest to a specific timestamp
-        auto targetTime = std::chrono::steady_clock::now() - 50ms;
-        data = buffer.get(targetTime);
-        std::cout << "Read data closest to timestamp: ";
-        std::apply([](auto &&... args)
-                   { ((std::cout << args << " "), ...); },
-                   data);
-        std::cout << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//        auto targetTime = std::chrono::steady_clock::now() - 200ms;
+//        data = buffer.get(targetTime);
+//        std::cout << "Read data closest to ts: ";
+//        std::apply([](auto &&... args)
+//                   { ((std::cout << args << " "), ...); },
+//                   data);
+//        std::cout << std::endl;
+//        std::cout << "----------------" << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 }
 
 int main()
 {
-    DoubleBuffer<std::tuple<int, float>, std::tuple<std::string, std::string>> buffer(20, 4); // Circular buffer size of 20 elements, thread pool size of 4
+    //DoubleBuffer<std::tuple<int, float>, std::tuple<std::string, std::string>> buffer(5, 1); // Circular buffer size of 20 elements, thread pool size of 4
+    DoubleBuffer<std::tuple<int, float>, std::tuple<std::string, std::string>> buffer;
+    buffer.set_buffer_size(10);
+
     std::thread producerThread(producer, std::ref(buffer));
     std::thread consumerThread(consumer, std::ref(buffer));
 
